c26b0fcf84a913e32756a3013b9a8f151d430594
Aditya <k.aditya00@gmail.com>
2019-04-24 00:54:30 +0530

    [Feat] Class method

--- company.py
+++ company.py
@@ -16,21 +16,27 @@ class Employee:
         self.email = first + '.' + last + '@company.com'
 
         # Incrementing the count of each employee when the constructor is called
-        # NOTE: We use <class name>.<class variable> syntax to acces the variable as we want the count_employee to be same in 
+        # NOTE: We use <class name>.<class variable> syntax to acces the variable as we want the count_employee to be same in
         # all the instances
-        Employee.count_employee += 1 
+        Employee.count_employee += 1
 
     def fullname(self):
         return f'{self.first} {self.last}'
 
     def apply_raise(self):
-        self.pay = self.pay * self.raise_amount  # Using self here as raise_amount may be different for different Employees
+        # Using self here as raise_amount may be different for different Employees
+        self.pay = self.pay * self.raise_amount
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        # Class method which changes the values of a class variable which is reflected in all the instances
+        # By convention, it should be called by the class itself (not the instance)
+        cls.raise_amount = amount
+
 
 employee_1 = Employee('Aditya', 'Kumar', 10000)
 employee_2 = Employee('Test', 'Name', 12000)
 
-print(employee_1.fullname())
-print(employee_2.fullname())
-
-print(employee_1.count_employee)
-print(Employee.count_employee)
+Employee.set_raise_amount(1.05)
+print(employee_1.raise_amount)
+print(employee_2.raise_amount)
